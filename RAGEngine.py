bb58575be444713fd681b4eccc7d3aeeb72b0888
Swathi Gnanasekar <88793179+swathisekar1411@users.noreply.github.com>
2024-12-03 17:34:24 -0800

    Add files via upload

---
+++ RAGEngine.py
@@ -0,0 +1,99 @@
+# nlp_utils.py
+import os
+import numpy as np
+import random
+import torch
+from tqdm import tqdm
+import pandas as pd
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+from langchain_chroma import Chroma
+from uuid import uuid4
+from langchain.docstore.document import Document
+
+
+def seed_everything(seed=0):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def setup_embeddings_and_store(
+    embeddings_model="BAAI/bge-small-en-v1.5", csv_path="./documents.csv"
+):
+    model_kwargs = {"device": "cpu"}
+    encode_kwargs = {"normalize_embeddings": True}
+    hf_embeddings = HuggingFaceEmbeddings(
+        model_name=embeddings_model,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs,
+    )
+
+    vector_store = Chroma(
+        collection_name="my_collection",
+        embedding_function=hf_embeddings,
+        persist_directory="./chroma_langchain_db",  # Where to save data locally, remove if not necessary
+    )
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators="\n\n",
+        chunk_size=1000,
+    )
+    if vector_store._collection.count() == 0:
+        print("Adding documents to the database...")
+
+        df = pd.read_csv(csv_path, index_col=0)
+
+        docs = []
+        for i, row in tqdm(df.iterrows()):
+            doc_chunks = text_splitter.split_text(row["text"])
+            docs.extend(
+                [
+                    Document(
+                        page_content=chunk,
+                        metadata={"index": i, "source_url": row["source_url"]},
+                    )
+                    for chunk in doc_chunks
+                ]
+            )
+
+        uuids = [str(uuid4()) for _ in range(len(docs))]
+        vector_store.add_documents(documents=docs, ids=uuids)
+    else:
+        print("Database already exists. Skipping document addition.")
+        print(f"total documents: {vector_store._collection.count()}")
+    return hf_embeddings, vector_store
+
+
+def setup_retrieval_chain(vector_store, llm_name="llama-3.2-1b-preview"):
+    groq_api_key = os.getenv("GROQ_API_KEY")
+    llm = ChatGroq(
+        groq_api_key=groq_api_key,
+        model_name=llm_name,
+    )
+    prompt = ChatPromptTemplate.from_template(
+        """
+    Answer the questions based on the provided context only.
+    Please provide the most accurate response based on the question
+    <context>
+    {context}
+    </context>
+    Questions:{input}
+    """
+    )
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = vector_store.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    return retrieval_chain
+
+
+def search_document(retrieval_chain, question):
+    results = retrieval_chain.invoke({"input": question})
+    return results
---
+++ app.py
@@ -0,0 +1,44 @@
+# app.py
+import streamlit as st
+from RAGEngine import (
+    seed_everything,
+    setup_embeddings_and_store,
+    setup_retrieval_chain,
+    search_document,
+)
+
+# Set the seed for reproducibility
+seed_everything(0)
+
+# Set up the embeddings and vector store
+hf_embeddings, vector_store = setup_embeddings_and_store(
+    embeddings_model="BAAI/bge-small-en-v1.5", csv_path="./documents.csv"
+)
+st.session_state.update({"hf_embeddings": hf_embeddings, "vector_store": vector_store})
+# Set up the retrieval chain
+retrieval_chain = setup_retrieval_chain(vector_store, llm_name="llama-3.2-1b-preview")
+
+# Streamlit UI
+st.title("Document Question Answering System")
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+
+question = st.chat_input("Ask a question:")
+
+if question:
+    with st.chat_message("user"):
+        st.write(question)
+    st.session_state.messages.append({"role": "user", "content": question})
+
+    with st.spinner("Retrieving answer..."):
+        results = search_document(retrieval_chain, question)
+        with st.chat_message("assistant"):
+            st.write(results["answer"])
+        st.session_state.messages.append({"role": "assistant", "content": results["answer"]})
